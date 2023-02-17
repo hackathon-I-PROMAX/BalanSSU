e12207b8a3507f295f5862bb1185ddd236e09dd6
LeeJoEun-01 <joeun1005@naver.com>
2023-02-17 13:53:00 +0900

    기본 세팅

---
+++ Balanssu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Balanssu
+//
+//  Created by 이조은 on 2023/02/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .systemPink
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
