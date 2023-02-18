b74e30dc7d3913b147d1442b47ac712cdd90beb4
LeeJoEun-01 <joeun1005@naver.com>
2023-02-18 20:50:01 +0900

    VoteList 페이지 제작
    badge + title + participant

--- Balanssu/Screens/VoteList/VoteListTableViewCell.swift
+++ Balanssu/Screens/VoteList/VoteListTableViewCell.swift
@@ -6,25 +6,62 @@
 //
 
 import UIKit
+import YDS
 
 class VoteListTableViewCell: UITableViewCell {
     
     static let identifier = "VoteListTableViewCell"
     
-    let voteTitle = UILabel()
+    let voteTitle: UILabel = {
+        let lable = UILabel()
+        lable.font = UIFont(name: "AppleSDGothicNeoR00", size: 17.0)
+        return lable
+    }()
+    
+    let badge: YDSBadge = {
+        let badge = YDSBadge()
+        badge.color = .blue
+        badge.layer.cornerRadius = 12
+        return badge
+    }()
+    
+    let participant: UILabel = {
+        let lable = UILabel()
+        lable.font = UIFont(name: "AppleSDGothicNeoR00", size: 12.0)
+        lable.textColor = UIColor(red: 0.249, green: 0.378, blue: 0.629, alpha: 1)
+        return lable
+    }()
+    
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
-        [voteTitle].forEach {
-            contentView.addSubview($0)
+        addContentView()
+        autoLayout()
+    }
+    
+    
+    private func addContentView() {
+        contentView.addSubview(badge)
+        contentView.addSubview(voteTitle)
+        contentView.addSubview(participant)
+    }
+        
+    private func autoLayout() {
+        badge.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalToSuperview().offset(20)
         }
         
-        voteTitle.snp.makeConstraints { (make) in
-            make.centerY.equalToSuperview()
-            make.leading.equalToSuperview().offset(20)
+        voteTitle.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalTo(badge.snp.trailing).offset(10)
         }
-            
+        
+        participant.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.trailing.equalToSuperview().offset(-5)
+        }
     }
         
     required init?(coder: NSCoder) {
