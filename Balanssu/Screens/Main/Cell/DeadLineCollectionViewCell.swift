77fb96592a45bab7e73f0b23c1f93b8857e19c76
김규철 <rbcjf122@gmail.com>
2023-02-19 11:18:27 +0900

    [#2] DeadLineCollectionView 구현 및 VC 연결

---
+++ Balanssu/Screens/Main/Cell/DeadLineCollectionViewCell.swift
@@ -0,0 +1,54 @@
+//
+//  DeadLineCollectionViewCell.swift
+//  Balanssu
+//
+//  Created by 김규철 on 2023/02/19.
+//
+
+import UIKit
+
+import SnapKit
+
+import Then
+
+class DeadLineCollectionViewCell: UICollectionViewCell {
+    
+    static let identifier = "DeadLineCollectionViewCell"
+    
+    let imageView = UIImageView().then {
+        $0.contentMode = .scaleToFill
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setViewHierarchy()
+        setConstraints()
+        configUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+    }
+    
+    func setViewHierarchy() {
+        contentView.addSubview(imageView)
+    }
+    
+    func setConstraints() {
+        imageView.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.leading.equalToSuperview()
+            $0.trailing.equalToSuperview()
+            $0.height.equalTo(148)
+        }
+    }
+    
+    func configUI() {
+        contentView.backgroundColor = .systemRed
+    }
+    
+}
