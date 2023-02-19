0639b5e2d46644418eeb028415dfa54e6a37ddb8
LeeJoEun-01 <joeun1005@naver.com>
2023-02-19 17:03:12 +0900

    CommentView 페이지 제작
    (추가) 작성 버튼 + 스크롤뷰 수정

--- Balanssu/Screens/Vote/CommentListTableViewCell.swift
+++ Balanssu/Screens/Vote/CommentListTableViewCell.swift
@@ -21,12 +21,22 @@ class CommentListTableViewCell: UITableViewCell {
     }()
     let name : UILabel = {
         let label = UILabel()
-        label.font = UIFont(name: "AppleSDGothicNeoB", size: 13.0)
+        label.font = UIFont(name: "AppleSDGothicNeoB00", size: 13.0)
         return label
     }()
+    let badge = BasePaddingLabel(padding: UIEdgeInsets(top: 4, left: 4, bottom: 4, right: 4)).then {
+            $0.backgroundColor = UIColor(red: 0.992, green: 0.969, blue: 0.898, alpha: 1)
+            $0.font = UIFont(name: "AppleSDGothicNeoR00", size: 11.0)
+            $0.textColor = UIColor(red: 0.746, green: 0.605, blue: 0.183, alpha: 1)
+            $0.clipsToBounds = true // 요소가 삐져나가지 않도록 하는 속성
+            $0.layer.cornerRadius = 4 // 둥글게 만드는 정도
+            $0.textAlignment = .center
+    }
     let comment : UILabel = {
         let label = UILabel()
-        label.font = UIFont(name: "AppleSDGothicNeoM", size: 15.0)
+        label.font = UIFont(name: "AppleSDGothicNeoR00", size: 15.0)
+        label.numberOfLines = 0
+        label.lineBreakMode = .byCharWrapping
         return label
     }()
     
@@ -40,24 +50,29 @@ class CommentListTableViewCell: UITableViewCell {
     private func addContentView() {
         contentView.addSubview(img)
         contentView.addSubview(name)
+        contentView.addSubview(badge)
         contentView.addSubview(comment)
     }
         
     private func autoLayout() {
+        
         img.snp.makeConstraints {
-            $0.top.leading.equalToSuperview().inset(10)
-            $0.size.width.height.equalTo(40)
+            $0.top.leading.equalToSuperview().inset(15)
+            $0.size.width.height.equalTo(32)
         }
         
         name.snp.makeConstraints {
             $0.top.equalToSuperview().inset(20)
-            $0.leading.equalTo(img.snp.trailing).offset(10)
-            
+            $0.leading.equalTo(img.snp.trailing).offset(7)
+        }
+        
+        badge.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(17)
+            $0.leading.equalTo(name.snp.trailing).offset(5)
         }
         
         comment.snp.makeConstraints {
-            $0.leading.equalToSuperview().inset(10)
-            $0.bottom.equalToSuperview().inset(10)
+            $0.edges.equalToSuperview().inset(UIEdgeInsets(top:50, left: 15, bottom: 10, right: 15))
         }
     }
         
@@ -77,3 +92,7 @@ class CommentListTableViewCell: UITableViewCell {
     }
 
 }
+
+//extension CommentListTableViewCell {
+//    public override func sizeThatFits(_ size: CGSize) -> CGSize
+//}
