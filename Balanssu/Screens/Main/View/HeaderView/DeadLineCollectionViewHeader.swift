16368691b7a3d4c10f670f49427f1f1078a8820a
김규철 <rbcjf122@gmail.com>
2023-02-19 12:40:40 +0900

    [#2-1] MainView, MyPageView 재 푸쉬

---
+++ Balanssu/Screens/Main/View/HeaderView/DeadLineCollectionViewHeader.swift
@@ -0,0 +1,62 @@
+//
+//  DeadLineCollectionViewHeader.swift
+//  Balanssu
+//
+//  Created by 김규철 on 2023/02/18.
+//
+
+import UIKit
+
+import SnapKit
+
+import Then
+
+class DeadLineCollectionViewHeader: UITableViewHeaderFooterView {
+
+    static let identifier = "DeadLineCollectionViewHeader"
+    
+    private let hotTitleLabel = UILabel().then {
+        $0.text = "마감된 밸런슈"
+        $0.font = UIFont.boldSystemFont(ofSize: 20)
+        $0.textColor = .black
+        $0.sizeToFit()
+    }
+    
+    let deadLineListButton = UIButton().then {
+        $0.setTitle("전체보기", for: .normal)
+        $0.titleLabel?.font = .systemFont(ofSize: 14)
+        $0.setTitleColor(UIColor(red: 0.249, green: 0.378, blue: 0.629, alpha: 1), for: .normal)
+    }
+
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+        setViewHierarchy()
+        setConstraints()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+     func setViewHierarchy() {
+        addSubview(hotTitleLabel)
+        addSubview(deadLineListButton)
+         
+    }
+     func setConstraints() {
+        
+         hotTitleLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(20)
+            $0.leading.equalToSuperview().inset(20)
+             $0.bottom.equalToSuperview().inset(17)
+        }
+         
+         deadLineListButton.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(20)
+            $0.trailing.equalToSuperview().inset(20)
+            $0.bottom.equalToSuperview().inset(17)
+        }
+         
+    }
+}
