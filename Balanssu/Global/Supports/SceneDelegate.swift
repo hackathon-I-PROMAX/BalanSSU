6b3e4b07af0d2dfaaef07e22de7d268325a6d840
김규철 <rbcjf122@gmail.com>
2023-02-24 00:13:43 +0900

    [#20] 자동로그인 관련 sceneDelegate 수정

--- Balanssu/Global/Supports/SceneDelegate.swift
+++ Balanssu/Global/Supports/SceneDelegate.swift
@@ -1,3 +1,5 @@
+
+
 //
 //  SceneDelegate.swift
 //  Balanssu
@@ -19,10 +21,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
+        var rootViewController: UIViewController
         
-        let mainViewController = MypageViewController()
-        let navigationController = UINavigationController(rootViewController: mainViewController)
-        window.rootViewController = navigationController // 시작 VC 작성해주기
+        if UserDefaults.standard.bool(forKey: UserDefaultKey.loginStatus) {
+            rootViewController = UINavigationController(rootViewController: MainViewController())
+        } else {
+            rootViewController = UINavigationController(rootViewController: StartViewController())
+        }
+        window.rootViewController = rootViewController // 시작 VC 작성해주기
         window.makeKeyAndVisible()
         self.window = window
     }
@@ -69,4 +75,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
 }
+
 
