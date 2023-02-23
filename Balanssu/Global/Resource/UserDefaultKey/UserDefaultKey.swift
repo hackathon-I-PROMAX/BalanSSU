5fdeee37b106f67d7e89f581741ae03369ca324c
김규철 <rbcjf122@gmail.com>
2023-02-23 17:05:40 +0900

    [#20] BlankData, Error Response 및 userDefault(token) DTO 추가

---
+++ Balanssu/Global/Resource/UserDefaultKey/UserDefaultKey.swift
@@ -0,0 +1,16 @@
+//
+//  UserDefaultKey.swift
+//  Balanssu
+//
+//  Created by 김규철 on 2023/02/22.
+//
+
+enum UserDefaultKey {
+    /// auth token
+    static let token = "token"
+
+    static let loginStatus = "loginStatus"
+
+    /// firebase token
+    // static let fcmToken = "fcmToken"
+}
---
+++ Balanssu/Network/DTO/ErrorResponse.swift
@@ -0,0 +1,15 @@
+//
+//  ErrorResponse.swift
+//  Balanssu
+//
+//  Created by 김규철 on 2023/02/21.
+//
+
+import Foundation
+
+struct ErrorResponse: Codable {
+    let timeStamp: String
+    let status: Int
+    let message: String
+    let error: String?
+}
