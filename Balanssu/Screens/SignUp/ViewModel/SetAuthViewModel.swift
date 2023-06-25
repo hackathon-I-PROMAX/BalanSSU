54e38e46fc893d2b26a28cb82f58e2cc458f56a8
김규철 <rbcjf122@gmail.com>
2023-06-26 00:42:45 +0900

    chore: 아이디 중복 검사 API 연결(#50)

--- Balanssu/Screens/SignUp/ViewModel/SetAuthViewModel.swift
+++ Balanssu/Screens/SignUp/ViewModel/SetAuthViewModel.swift
@@ -13,12 +13,19 @@ import RxCocoa
 class SetAuthViewModel:ViewModelType {
 
     var disposeBag = DisposeBag()
+    private let authDataSource: DefaultAuthDataSource
+    var booll = Bool()
+
+    init(authDataSource: DefaultAuthDataSource) {
+        self.authDataSource = authDataSource
+    }
     
     struct Input {
         let didIdTextFieldChange: Observable<String>
         let didPasswordTextFieldChange: Observable<String>
         let didPasswordCheckTextFieldChange: Observable<String>
         let didPasswordTextFieldDidTapEvent: Observable<Void>
+        let didIdValidationEvent: Observable<Void>
         let tapConfirmButton: Observable<Void>
 //        let tapBackButton: Signal<Void>
     }
@@ -26,19 +33,20 @@ class SetAuthViewModel:ViewModelType {
     struct Output { 
 //        let didBackButtonTapped: Signal<Void>
         let isIdValid = PublishRelay<Bool>()
+        let id = BehaviorRelay<String>(value: "")
+        let didIdDuplicateCheck = PublishRelay<Bool>()
         let isPasswordValid = PublishRelay<Bool>()
         let isPasswordCheckValid = PublishRelay<Bool>()
         let isAllInputValid = PublishRelay<Bool>()
         let didConfirmButtonTapped = PublishRelay<(String, String)>()
+        let validationErrorMessage = BehaviorRelay<String>(value: "")
     }
     
     
     func transform(input: Input) -> Output {
         let output = Output()
-        
-        let isPasswordCheckValid = Observable.combineLatest(input.didPasswordTextFieldChange, input.didPasswordCheckTextFieldChange)
-        
-        let isAllInputValid = Observable.combineLatest(output.isIdValid, output.isPasswordValid, output.isPasswordCheckValid, resultSelector: { $0 && $1 && $2 })
+
+        let isAllInputValid = Observable.combineLatest(output.isIdValid, output.isPasswordValid, output.isPasswordCheckValid, output.didIdDuplicateCheck, resultSelector: { $0 && $1 && $2 && $3 })
         
         input.didIdTextFieldChange
             .distinctUntilChanged()
@@ -51,6 +59,20 @@ class SetAuthViewModel:ViewModelType {
             })
             .disposed(by: disposeBag)
         
+        input.didIdTextFieldChange
+            .subscribe(onNext: { text in
+                output.id.accept(text)
+            })
+            .disposed(by: disposeBag)
+        
+        input.didIdValidationEvent
+            .withUnretained(self)
+            .flatMapLatest { owner, _ in
+                owner.checkIdValid(userName: output.id.value)
+            }
+            .bind(to: output.didIdDuplicateCheck)
+            .disposed(by: disposeBag)
+    
         input.didPasswordTextFieldChange
             .distinctUntilChanged()
             .withUnretained(self)
@@ -103,4 +125,27 @@ class SetAuthViewModel:ViewModelType {
         let passwordPredicate = NSPredicate(format: "SELF MATCHES %@", passwordRegex)
         return passwordPredicate.evaluate(with: password)
     }
+    
+    private func checkIdValid(userName: String) -> Single<Bool> {
+        let output = Output()
+        return authDataSource.postValidationId(userName: userName)
+            .map { result -> Bool in
+                switch result {
+                case .success:
+                    return true
+                case .failure(let error):
+                    switch error {
+                    case .requestErr(let err):
+                        output.validationErrorMessage.accept(err.message ?? "")
+                    default:
+                        print(error)
+                    }
+                    return false
+                }
+            }
+            .catchAndReturn(false)
+    }
 }
+
+
+
