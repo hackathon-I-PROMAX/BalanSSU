8c8b7b8e7954730f2ce7a4fc214ee74bc89e836c
Bibi-urssu <bibi.urssu@gmail.com>
2023-07-24 13:26:26 +0900

    [64-MBTI] 회원가입 학과 -> MBTI 수정

--- Balanssu/Screens/SignUp/SignUp2ViewController.swift
+++ Balanssu/Screens/SignUp/SignUp2ViewController.swift
@@ -41,19 +41,7 @@ class SignUp2ViewController: BaseViewController, UITextFieldDelegate, UIPickerVi
     
     let gender = ["M", "F"]
     let grade = ["23", "22", "21", "20", "19", "18", "17", "16", "15"]
-    let major = ["기독교학과", "국어국문학과", "영어영문학과", "독어독문학과", "불어불문학과",
-                 "중어중문학과", "일어일문학과", "철학과", "사학과", "예술창작학부 문예창작전공",
-                 "예술창작학부 영화예술전공", "스포츠 학부", "수학과", "물리학과",
-                 "화학과", "정보통계보험수리학과", "의생명시스템학부", "법학과",
-                 "국제법무학과", "화학공학과", "유기신소재파이버공학과", "전기공학부",
-                 "기계공학부", "산업정보시스템공학과", "건축학부", "경제학과",
-                 "글로벌통상학과", "사회복지학부", "행정학부", "정치외교학과",
-                 "정보사회학과", "언론홍보학과", "평생교육학과", "경영학부",
-                 "회계학과", "벤처중소기업학과", "금융학부", "벤처경영학과",
-                 "혁신경영학과", "복지경영학과", "회계세무학과", "컴퓨터학부",
-                 "전자정보공학부 전자공학전공", "전자정보공학부 IT융합전공",
-                 "글로벌미디어학부", "소프트웨어학부", "AI 융합학부",
-                 "미디어경영학과", "융합특성화 자유전공학부","차세대반도체학과건축학부"]
+    let major = ["ENTP", "ENFP", "ESTP", "ESFP", "ENTJ", "ENFJ", "ESTJ", "ESFJ", "INTP", "INFP", "ISTP", "ISFP", "INTJ", "INFJ", "ISTJ", "ISFJ"]
     
     func createGenderPickerView() {
         let genderPickerView = UIPickerView()
@@ -211,7 +199,7 @@ class SignUp2ViewController: BaseViewController, UITextFieldDelegate, UIPickerVi
     }
     
     let majorLabel = UILabel().then {
-        $0.text = "학부"
+        $0.text = "MBTI"
         $0.textColor = .black
         $0.font = UIFont(name: "AppleSDGothicNeoM00", size: 16)
     }
@@ -240,7 +228,7 @@ class SignUp2ViewController: BaseViewController, UITextFieldDelegate, UIPickerVi
     }
     
     let majorTextField = UITextField().then {
-        $0.placeholder = "학부를 선택해주세요"
+        $0.placeholder = "MBTI를 선택해주세요"
         $0.backgroundColor = UIColor(r: 248, g: 248, b: 248)
         $0.layer.cornerRadius = 8
         $0.tintColor = .clear
