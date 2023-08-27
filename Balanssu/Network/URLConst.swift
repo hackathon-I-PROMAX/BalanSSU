1f9581b6ce6e5c95eff4f21890b638d78e1006f6
Bibi-urssu <bibi.urssu@gmail.com>
2023-08-28 02:36:21 +0900

    add: getReport api 추가

--- Balanssu/Network/URLConst.swift
+++ Balanssu/Network/URLConst.swift
@@ -29,6 +29,7 @@ enum URLConst {
     // MARK: - VoteView
     static let voteView = "/categories"
     static let comment = "/comments"
+    static let report = "/categories"
 
     // MARK: - Choices
     static let choices = "/choices"
@@ -43,3 +44,4 @@ enum URLConst {
     static let withdrawal = "/auth/withdrawal"
     
 }
+
